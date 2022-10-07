dbba5365866a6c0bfdb94b9ed502050a3558dcc7
Chamika Weerasekara <chamikashe@gmail.com>
2022-10-07 13:25:17 +0530

    splash ui and functions added

--- app/src/main/java/com/digikraft/bikestation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/digikraft/bikestation/ui/splash/SplashActivity.kt
@@ -1,12 +1,36 @@
 package com.digikraft.bikestation.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.digikraft.bikestation.R
+import com.digikraft.bikestation.databinding.ActivitySplashBinding
+import com.digikraft.bikestation.ui.home.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private var _binding: ActivitySplashBinding? = null
+    private val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        _binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        openMainView()
+    }
+
+    private fun openMainView() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
