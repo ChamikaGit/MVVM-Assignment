4c2afa87d99783aafa1c2aab158c6effb8eaf2ff
Chamika Weerasekara <chamikashe@gmail.com>
2022-10-07 11:46:59 +0530

    inital code commit

---
+++ app/src/main/java/com/digikraft/bikestation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.digikraft.bikestation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
